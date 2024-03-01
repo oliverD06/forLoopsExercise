b4fded08de7309958a7519105de2eeb07aa1ce37
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2024-03-01 09:32:22 -0600

    Exercise completed.

---
+++ script.js
@@ -0,0 +1,19 @@
+for (i = 1; i <= 7; i++) {
+    console.log(i)
+}
+
+for (i = 5; i <=25; i = i + 4) {
+    console.log(i)
+}
+
+const wizards = ["Harry Potter", "Hermione Granger", "Ron Weasley"]
+for (wizard of wizards) {
+    console.log(wizard)
+}
+
+let harryPotterMovies = 0
+while (harryPotterMovies != 8) {
+    harryPotterMovies = harryPotterMovies + 1
+    
+}
+console.log(harryPotterMovies)
